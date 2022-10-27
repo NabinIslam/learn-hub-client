c1cb3c3e564d59484dbbe0605d8a4fa64e9f7355
Nabin Islam <sinabin62246@gmail.com>
2022-10-27 15:53:24 +0600

    faq page done

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,5 +1,77 @@
 const Faq = () => {
-  return <div>Faq</div>;
+  return (
+    <div className="py-10">
+      <div className="container mx-auto">
+        <div className="mb-8">
+          <h2 className="font-bold text-3xl">
+            What do LearnHub courses include?
+          </h2>
+          <p className="font-semibold">
+            Each LearnHub course is created, owned and managed by the
+            instructor(s). The foundation of each LearnHub course are its
+            lectures, which can include videos, slides, and text. In addition,
+            instructors can add resources and various types of practice
+            activities, as a way to enhance the learning experience of students.{' '}
+          </p>
+        </div>
+        <div className="mb-8">
+          <h2 className="font-bold text-3xl">
+            How do I take a LearnHub course?
+          </h2>
+          <p className="font-semibold">
+            LearnHub courses are entirely on-demand and they can be accessed
+            from several different devices and platforms, including a desktop,
+            laptop, and our mobile app. After you enroll in a course, you can
+            access it by clicking on the course link you will receive in your
+            confirmation email (provided you’re logged into your LearnHub
+            account). You can also begin the course by logging in and navigating
+            to your My learning page.
+          </p>
+        </div>
+        <div className="mb-8">
+          <h2 className="font-bold text-3xl">
+            Do I have to start my LearnHub course at a certain time? And how
+            long do I have to complete it?
+          </h2>
+          <p className="font-semibold">
+            There are no deadlines to begin or complete a course. Learn more
+            about our Lifetime Access policy.
+          </p>
+        </div>
+        <div className="mb-8">
+          <h2 className="font-bold text-3xl">
+            Is LearnHub an accredited institution? Do I receive anything after I
+            complete a course?
+          </h2>
+          <p className="font-semibold">
+            While LearnHub is not an accredited institution, we offer
+            skills-based courses taught by real-world experts in their field.
+            Every approved, paid course features a certificate of completion to
+            document your accomplishment.
+          </p>
+        </div>
+        <div className="mb-8">
+          <h2 className="font-bold text-3xl">
+            Is there any way to preview a course?
+          </h2>
+          <p className="font-semibold">
+            Yes! Learn how to preview a course, and review key information about
+            it. This article outlines how you can search LearnHub’s ever-growing
+            library, and find courses you’re interested in taking.
+          </p>
+        </div>
+        <div className="mb-8">
+          <h2 className="font-bold text-3xl">How can I pay for a course?</h2>
+          <p className="font-semibold">
+            We want you to be satisfied, so all eligible courses purchased on
+            LearnHub can be refunded within 30 days. If you are unhappy with a
+            course, you can request a refund, provided the request meets the
+            guidelines in our refund policy.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
